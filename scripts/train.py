098945a0dc30dfa78e48980df35942b4178695b0
adnansanaulla <adnansanaulla7860@gmail.com>
2025-08-06 22:28:20 -0400

    training comments

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,7 @@ from agents.dqn_agent import DQNAgent
 from env.turtlebot_env import TurtleBotEnv
 import numpy as np
 import yaml
+## training
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
